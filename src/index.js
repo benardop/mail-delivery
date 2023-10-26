0bac60f2b76c48eb1747a7b4dff8c0532b0e2eeb
benardop <benard.oc.p@gmail.com>
2023-10-26 19:25:30 -0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+function MailDelivery() {
+  return (
+    <div className='mail-delivery'>
+      <div className='sender-address'>
+      <SenderName />
+      <SenderStreet />
+      <SenderState />
+      </div>
+      <div className='receiver-address'>
+      <ReceiverName />
+      <ReceiverStreet />
+      <ReceiverState />
+      </div>
+      <div className='stamp'>
+        <PostageStamp />
+      </div>
+    </div>
+  )
+}
+
+function SenderName() {
+  return (
+    <span>John Doe</span>
+  )
+}
+
+function SenderStreet() {
+  return (
+    <span>North Street 100</span>
+  )
+}
+
+function SenderState() {
+  return (
+    <span>California-CA</span>
+  )
+}
+
+function ReceiverName() {
+  return (
+    <span>Alex Doe</span>
+  )
+}
+
+function ReceiverStreet() {
+  return (
+    <span>Gold Park 1000</span>
+  )
+}
+
+function ReceiverState() {
+  return (
+    <span>California-CA</span>
+  )
+}
+
+function PostageStamp() {
+  return (
+    <div>
+      <FontAwesomeIcon icon={faHeart} size="2x" color="black" />
+    </div>
+  )
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MailDelivery />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
