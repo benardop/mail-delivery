ab330b4c8a91917d79f62822dff5a0a9e943909a
benardop <benard.oc.p@gmail.com>
2023-11-07 08:30:24 -0300

    Add AddressLabel componet

--- src/index.js
+++ src/index.js
@@ -1,44 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import stamp from './image/stamp.jpg';
+import stamp from './image/stamp.jpg';
 import './index.css';
 
 function AddressLabel({person}) {
   return (
    <div className='address-label'>
-      <Name text={person.name} />
-      <Address streetName={person.address} />
-      <State stateName={person.state} />
+      <FromPerson fromPerson={person.fromPerson} />
+      <ToPerson toPerson={person.toPerson} />
+      <Stamp/>
    </div> 
   )
 }
-function Name({text}) {
+function FromPerson({fromPerson}) {
+  const { name, address, state } = fromPerson;
   return (
-    <div>
-      {text}
+    <div className='sender-Adddress'>
+      <div className='name'>{name}</div>
+      <div className='street'>{address}</div>
+      <div className='state'>{state}</div>
     </div>
   )
 }
-function Address({streetName}) {
+function ToPerson({toPerson}) {
+  const { name, address, state } = toPerson;
   return (
-    <div>
-      {streetName}
+    <div className='receiver-address'>
+      <div className='name'>{name}</div>
+      <div className='street'>{address}</div>
+      <div className='state'>{state}</div>
     </div>
   )
 }
 
-function State({stateName}) {
+function Stamp() { 
   return (
-    <div>
-      {stateName}
+    <div className='postage-stamp'>
+      <img 
+        src={stamp}
+        className='stamp-image'
+        alt='Pastage stamp' />
     </div>
   )
 }
 
 var testPerson = {
-  name: "John Doe",
-  address: "Wood lane 100-00900",
-  state:"Boston MA, 02118"
+  fromPerson: {
+    name: "John Doe",
+    address: "Wood lane 100-00900",
+    state:"Boston MA, 02118"
+  },  
+  toPerson: {
+    name: "Alex Doe",
+    address: "Wood lane 100-00900",
+    state:"Carlifornia CA, 02118"
+  }
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
