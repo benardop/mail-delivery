28aaf5e07ebd00dd35eea862b8fddbbd0d633a93
benardop <benard.oc.p@gmail.com>
2024-01-11 16:05:57 -0300

    Update the main component with usestate hooks

--- src/index.js
+++ src/index.js
@@ -1,66 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import stamp from './image/stamp.jpg';
-import './index.css';
+import { useState } from 'react';
 
-function AddressLabel({person}) {
-  return (
-   <div className='address-label'>
-      <FromPerson fromPerson={person.fromPerson} />
-      <ToPerson toPerson={person.toPerson} />
-      <Stamp/>
-   </div> 
-  )
-}
-function FromPerson({fromPerson}) {
-  const { name, address, state } = fromPerson;
-  return (
-    <div className='sender-Adddress'>
-      <div className='name'>{name}</div>
-      <div className='street'>{address}</div>
-      <div className='state'>{state}</div>
-    </div>
-  )
-}
-function ToPerson({toPerson}) {
-  const { name, address, state } = toPerson;
-  return (
-    <div className='receiver-address'>
-      <div className='name'>{name}</div>
-      <div className='street'>{address}</div>
-      <div className='state'>{state}</div>
-    </div>
-  )
-}
+function OneTimeButton({onClick}) {
+ const [clicked, setClicked] = useState(false);
 
-function Stamp() { 
-  return (
-    <div className='postage-stamp'>
-      <img 
-        src={stamp}
-        className='stamp-image'
-        alt='Pastage stamp' />
-    </div>
-  )
-}
+  const handleClick = () => {
 
-var testPerson = {
-  fromPerson: {
-    name: "John Doe",
-    address: "Wood lane 100-00900",
-    state:"Boston MA, 02118"
-  },  
-  toPerson: {
-    name: "Alex Doe",
-    address: "Wood lane 100-00900",
-    state:"Carlifornia CA, 02118"
-  }
-};
+    onClick();
+
+    setClicked(true);
+  };
+
+      return(
+        <button onClick={handleClick} disabled={clicked}>
+          You can only click me once
+        </button>
+      );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <AddressLabel person={testPerson}/>
-  </React.StrictMode>
+  <>
+    <OneTimeButton onClick={()=> alert('Hi')} />
+  </>
 );
 
