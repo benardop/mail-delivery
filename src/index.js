7ab760e175bdff067c3b6f7935068f88b20139e7
benardop <benard.oc.p@gmail.com>
2024-01-22 11:42:16 -0300

    Update index file components

--- src/index.js
+++ src/index.js
@@ -1,60 +1,25 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
+import { useState } from 'react';
 
-class CountingParent extends Component {
- constructor(props) {
-  super(props);
+function CountingParent() {
 
-  this.state = {
-    actionCount: 0
-  }
+  const [count, setCount] = useState(0);
 
-  this.handleAction = this.handleAction.bind(this);
-  this.resetButton = this.resetButton.bind(this);
- }
- handleAction() {
-  this.setState({
-    actionCount: this.state.actionCount + 1
-  },function(){    
-  });
+  function handleAction() {
+  setCount(count + 1);
  }
 
- resetButton(){
-   this.setState((state, props) => {
-    return {
-      actionCount: state.actionCount - 1
-    }
-   }
-  )
-  }
- render(){
-  return(
-    <div>
-      <Child onAction={this.handleAction} />
-      &nbsp;
-      <Reset onAction={this.resetButton}/>
-      <p>Clicked counter {this.state.actionCount} times!</p>
-    </div>
-  )
+ return(
+  <button onClick={handleAction}>
+    Clicked { count } times
+  </button>
+ )
+ 
  }
-}
+ 
 
-function Child({onAction}) {
-  return(
-    <button onClick={onAction}>
-      Set Count!
-    </button>
-  )
-}
-
-function Reset({onAction}) {
-  return(
-    <button onClick={onAction}>
-      Reset Count!
-    </button>
-  )
-}
-
+ 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
